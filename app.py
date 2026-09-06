92e4af71e252dfaedfcec1ef445b02f897f22032
Suvitfalcon <falconsuvit@gmail.com>
2026-09-06 20:06:18 +0530

    mark4

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# -----------------------------
+
 # STREAMLIT UI
 # -----------------------------
 
